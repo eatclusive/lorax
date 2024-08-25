ae58a004545548219953f2f1e7cf9308fd300e2c
Faizan <117235467+fazenrazen@users.noreply.github.com>
2024-08-25 17:46:44 -0400

    Revert "Django GUI and PostgreSQL for the database"

---
+++ gui.cpp
@@ -0,0 +1,17 @@
+/*
+    Eatclusive LLC
+    Edited by: Faizan Darsot
+    Edited date: 07/29/24
+*/ 
+
+// Use some simple gui framework or just use the terminal to test
+
+#include <iostream>
+
+void hello(); 
+
+int main () {
+    std::cout << "Hello Khulan";
+        
+    return 0;
+}   
